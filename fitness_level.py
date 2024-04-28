2dfa1253138b16a1332d9dac38f881e03a7cb81f
worldofdatascience <worldofdatascience@outlook.com>
2024-04-28 14:58:59 +0100

    create overall fitness score

--- fitness_level.py
+++ fitness_level.py
@@ -22,7 +22,41 @@ class FitnessIndicator:
         normalized_score = int(max(((score - min_score)/ \
                                       (max_score - min_score)), 0) * 100)
         return normalized_score
-    
+
+    def calculate_average_scores(self, *args):
+        if not args:
+            return 0
+        total = sum(args)
+        average = round(total / len(args), 0)
+        return average
+
+    def overall_score(self):
+        push_up_norm = self.calculate_normalize_score(self.push_up, 15, 99)
+        pull_up_norm = self.calculate_normalize_score(self.pull_up, 5, 37)
+        squat_norm = self.calculate_normalize_score(self.squats, 16, 178)
+        fivekm_time_norm = self.calculate_normalize_score(self.fivekm_time, 31.5, 19.75)
+        crunches_norm = self.calculate_normalize_score(self.crunches, 23, 159)
+
+        overall_score = self.calculate_average_scores(
+            push_up_norm,
+            pull_up_norm,
+            squat_norm,
+            fivekm_time_norm,
+            crunches_norm
+            )
+        
+        result = {
+            'overall_score': overall_score,
+            'push_up_norm': push_up_norm,
+            'pull_up_norm': pull_up_norm,
+            'squat_norm': squat_norm,
+            'fivekm_time_norm': fivekm_time_norm,
+            'crunches_norm': crunches_norm
+            }
+
+        return result
 
 file_location = 'test/demo.json'
 fitness_indicator = FitnessIndicator(file_location)
+fitness_scores = fitness_indicator.overall_score()
+print(fitness_scores)
